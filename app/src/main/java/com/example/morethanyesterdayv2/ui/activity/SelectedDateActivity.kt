9045d04328ee1f80d8ce350eae4d6b445814c8a8
DK <spol@naver.com>
2023-03-20 19:45:34 +0900

    SelectExerciseActivity.kt 추가

--- app/src/main/java/com/example/morethanyesterdayv2/ui/activity/SelectedDateActivity.kt
+++ app/src/main/java/com/example/morethanyesterdayv2/ui/activity/SelectedDateActivity.kt
@@ -1,12 +1,22 @@
 package com.example.morethanyesterdayv2.ui.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.morethanyesterdayv2.R
+import com.example.morethanyesterdayv2.databinding.ActivitySelectedDateBinding
 
 class SelectedDateActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySelectedDateBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_selected_date)
+        binding = ActivitySelectedDateBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        binding.SelectExerciseBtn.setOnClickListener {
+            startActivity(Intent(this, SelectExerciseActivity::class.java))
+        }
     }
 }
