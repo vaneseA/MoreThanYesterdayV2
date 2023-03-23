dc9ef44e48632ef3a14b3201442f42d79643a3dd
DK <spol@naver.com>
2023-03-23 20:25:14 +0900

    room 퍼왔음 수정시작

--- app/src/main/java/com/example/morethanyesterdayv2/ui/activity/SelectedDateActivity.kt
+++ app/src/main/java/com/example/morethanyesterdayv2/ui/activity/SelectedDateActivity.kt
@@ -1,22 +1,73 @@
 package com.example.morethanyesterdayv2.ui.activity
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.room.Room
+import com.example.morethanyesterdayv2.AboutRoom.RecyclerAdapter
+import com.example.morethanyesterdayv2.AboutRoom.RoomHelper
+import com.example.morethanyesterdayv2.AboutRoom.RoomMemo
+import com.example.morethanyesterdayv2.AboutRoom.RoomMemoDAO
 import com.example.morethanyesterdayv2.databinding.ActivitySelectedDateBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class SelectedDateActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivitySelectedDateBinding
+    val binding by lazy { ActivitySelectedDateBinding.inflate(layoutInflater) }
+    lateinit var helper: RoomHelper
+    lateinit var memoAdapter: RecyclerAdapter
+    val memoList = mutableListOf<RoomMemo>()
+    lateinit var memoDAO: RoomMemoDAO
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivitySelectedDateBinding.inflate(layoutInflater)
-
         setContentView(binding.root)
 
-        binding.SelectExerciseBtn.setOnClickListener {
-            startActivity(Intent(this, SelectExerciseActivity::class.java))
+        helper =
+            Room.databaseBuilder(this, RoomHelper::class.java, "room_db")
+//                .allowMainThreadQueries()//공부할때만 쓴다
+                .build()
+        memoDAO = helper.roomMemoDao()
+
+        memoAdapter = RecyclerAdapter(memoList)
+
+        refreshAdapter()
+
+        with(binding) {
+            recyclerMemo.adapter = memoAdapter
+//            recyclerMemo.layoutManager = LinearLayoutManager(baseContext)
+            recyclerMemo.layoutManager = LinearLayoutManager(this@SelectedDateActivity)
+
+            buttonSave.setOnClickListener {
+                val content = editMemo.text.toString()
+                if (content.isNotEmpty()) {
+                    val datetime = System.currentTimeMillis()
+                    val memo = RoomMemo(content, datetime)
+                    editMemo.setText("")
+                    insertMemo(memo)
+                }
+            }
+        }
+    }
+
+    fun insertMemo(memo: RoomMemo) {
+        CoroutineScope(Dispatchers.IO).launch {
+            memoDAO.insert(memo)
+            refreshAdapter()
+        }
+    }
+
+    fun refreshAdapter() {
+        CoroutineScope(Dispatchers.IO).launch {
+            memoList.clear()
+            memoList.addAll(memoDAO.getAll())
+            withContext(Dispatchers.Main) {
+                memoAdapter.notifyDataSetChanged()
+            }
         }
     }
 }
