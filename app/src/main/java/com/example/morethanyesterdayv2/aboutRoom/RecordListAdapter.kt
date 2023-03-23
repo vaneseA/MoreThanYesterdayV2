a3eededc5690c6d638bc062845ae38788b26fd17
DK <spol@naver.com>
2023-03-24 03:16:42 +0900

    커스텀 다이얼로그 운동 이름,운동 종류 추가

--- app/src/main/java/com/example/morethanyesterdayv2/aboutRoom/RecordListAdapter.kt
+++ app/src/main/java/com/example/morethanyesterdayv2/aboutRoom/RecordListAdapter.kt
@@ -3,7 +3,9 @@ package com.example.morethanyesterdayv2.aboutRoom
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 import com.example.morethanyesterdayv2.R
 import com.example.morethanyesterdayv2.databinding.RecordRvItemBinding
@@ -35,13 +37,17 @@ class RecordListAdapter(
                 TypeArea.text = exerciseEntity.exerciseType
 
                 addSetBtn.setOnClickListener {
-                    addSetDialog(it.context)
+                    addSetDialog(
+                        it.context,
+                        exerciseEntity.exerciseName,
+                        exerciseEntity.exerciseType
+                    )
                 }
             }
 
         }
 
-        private fun addSetDialog(context: Context) {
+        private fun addSetDialog(context: Context, name: String, type: String) {
             val dialogView =
                 LayoutInflater.from(context)
                     .inflate(R.layout.custom_add_set_dialog, null)
@@ -49,6 +55,15 @@ class RecordListAdapter(
                 .setView(dialogView)
 
             val alertDialog = builder.show()
+
+            //binding 전까지는 이거
+            val dialogExerciseName = alertDialog.findViewById<TextView>(R.id.dialogExerciseName)
+            val dialogExerciseType = alertDialog.findViewById<TextView>(R.id.dialogExerciseType)
+            dialogExerciseType
+            dialogExerciseType
+
+            dialogExerciseName?.text = name
+            dialogExerciseType?.text = type
         }
     }
 
