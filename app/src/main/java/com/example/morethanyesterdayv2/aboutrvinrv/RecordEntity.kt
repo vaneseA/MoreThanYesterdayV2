336fae34c85e549fac82cde8f65fb93fd1eaf1c1
DK <spol@naver.com>
2023-03-28 13:27:20 +0900

    뷰모델 만드는중

--- app/src/main/java/com/example/morethanyesterdayv2/aboutrvinrv/RecordEntity.kt
+++ app/src/main/java/com/example/morethanyesterdayv2/aboutrvinrv/RecordEntity.kt
@@ -7,25 +7,25 @@ import androidx.room.PrimaryKey
 @Entity(tableName = "room_record")
 class RecordEntity {
     @PrimaryKey(autoGenerate = true)// no에 값이 없을 때 자동증가된 숫자 값을 db에 입력해준다.
-    @ColumnInfo
+    @ColumnInfo(name = "no")
     var setNo: Long? = null
 
 
     //  운동이름
-    @ColumnInfo
+    @ColumnInfo(name = "exerciseName")
     var recordName: String = ""
 
     // 운동타입
-    @ColumnInfo
+    @ColumnInfo(name = "exerciseType")
     var recordType: String = ""
 
 
     // 중량
-    @ColumnInfo
+    @ColumnInfo(name = "totalKG")
     var kg: String = ""
 
     // 횟수
-    @ColumnInfo
+    @ColumnInfo(name = "totalCount")
     var count: String = ""
 
 
