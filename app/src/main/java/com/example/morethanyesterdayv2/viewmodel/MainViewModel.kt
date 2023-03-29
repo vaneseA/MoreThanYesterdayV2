527da2066744e46c6320de005a102e380bcf5570
DK <spol@naver.com>
2023-03-30 00:16:29 +0900

    임시 커밋2

--- app/src/main/java/com/example/morethanyesterdayv2/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/morethanyesterdayv2/viewmodel/MainViewModel.kt
@@ -6,6 +6,5 @@ import androidx.lifecycle.ViewModel
 class MainViewModel : ViewModel() {
     var selectedDate: String = ""
     var fname: String = ""
-    var str: String = ""
     // ... 추가적으로 ViewModel에서 유지할 변수 및 함수를 선언합니다.
 }
