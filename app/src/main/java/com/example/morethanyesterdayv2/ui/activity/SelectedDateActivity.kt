bad26fc98e018b39b59dc92b0edaf53196d03218
DK <spol@naver.com>
2023-03-30 18:11:05 +0900

    코드정리2

--- app/src/main/java/com/example/morethanyesterdayv2/ui/activity/SelectedDateActivity.kt
+++ app/src/main/java/com/example/morethanyesterdayv2/ui/activity/SelectedDateActivity.kt
@@ -20,7 +20,7 @@ import com.example.morethanyesterdayv2.dialog.CustomDialog
 
 class SelectedDateActivity : AppCompatActivity(), AddSetDialogInterface {
 
-
+//  by lazy는 프로퍼티 초기화를 지연시켜서 액티비티나 프래그먼트의 뷰가 생성될 때 불필요한 리소스 초기화를 피하고 애플리케이션 성능을 향상시킬 수 있다
     val binding by lazy { ActivitySelectedDateBinding.inflate(layoutInflater) }
     lateinit var helper: RoomHelper
     lateinit var parentAdapter: ParentAdapter
