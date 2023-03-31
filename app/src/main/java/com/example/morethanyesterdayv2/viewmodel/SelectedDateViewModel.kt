f62c1a9ac520aae5b7dff4b0c83a82f39c3cadc0
DK <spol@naver.com>
2023-03-31 21:17:02 +0900

    임시 커밋3

--- app/src/main/java/com/example/morethanyesterdayv2/viewmodel/SelectedDateViewModel.kt
+++ app/src/main/java/com/example/morethanyesterdayv2/viewmodel/SelectedDateViewModel.kt
@@ -30,6 +30,10 @@ class SelectedDateViewModel(application: Application) : AndroidViewModel(applica
         return exerciseList
     }
 
+    // 비동기적으로 DAO를 이용하여 그 날짜의 운동 정보를 가져와(getAllByDate(selectedDate))
+    // coroutine viewModelScope
+    // UI에 반영하는 작업을 수행, 이 작업은 백그라운드 스레드에서 실행되며,
+    // UI의 반응성과 앱의 안정성을 유지하기 위해 메인 스레드에서 UI를 업데이트.
     fun loadExerciseListByDate(selectedDate: String) {
         viewModelScope.launch(Dispatchers.IO) {
             val list = exerciseRepository.getAllByDate(selectedDate)
@@ -38,7 +42,10 @@ class SelectedDateViewModel(application: Application) : AndroidViewModel(applica
             }
         }
     }
-
+    // 비동기적으로 DAO를 이용하여 그 날짜와 특정 운동 정보를 가져와(getExerciseSetListByDateAndName(selectedDate, exerciseName))
+    // coroutine viewModelScope
+    // UI에 반영하는 작업을 수행, 이 작업은 백그라운드 스레드에서 실행되며,
+    // UI의 반응성과 앱의 안정성을 유지하기 위해 메인 스레드에서 UI를 업데이트.
     fun loadExerciseSetListByDateAndName(selectedDate: String, exerciseName: String) {
         viewModelScope.launch(Dispatchers.IO) {
             val list = selectedDateRepository.getExerciseSetListByDateAndName(selectedDate, exerciseName)
@@ -57,26 +64,26 @@ class SelectedDateViewModel(application: Application) : AndroidViewModel(applica
         }
     }
 
-    fun deleteExercise(entity: ExerciseEntity) {
-        viewModelScope.launch(Dispatchers.IO) {
-            exerciseDAO.delete(entity)
-            loadExerciseList()
-        }
-    }
-
-    fun updateExercise(entity: ExerciseEntity) {
-        viewModelScope.launch(Dispatchers.IO) {
-            exerciseDAO.update(entity)
-            loadExerciseList()
-        }
-    }
-
-    fun insertExercise(entity: ExerciseEntity) {
-        viewModelScope.launch(Dispatchers.IO) {
-            exerciseDAO.insert(entity)
-            loadExerciseList()
-        }
-    }
+//    fun deleteExercise(entity: ExerciseEntity) {
+//        viewModelScope.launch(Dispatchers.IO) {
+//            exerciseDAO.delete(entity)
+//            loadExerciseList()
+//        }
+//    }
+//
+//    fun updateExercise(entity: ExerciseEntity) {
+//        viewModelScope.launch(Dispatchers.IO) {
+//            exerciseDAO.update(entity)
+//            loadExerciseList()
+//        }
+//    }
+//
+//    fun insertExercise(entity: ExerciseEntity) {
+//        viewModelScope.launch(Dispatchers.IO) {
+//            exerciseDAO.insert(entity)
+//            loadExerciseList()
+//        }
+//    }
 
 
 }
