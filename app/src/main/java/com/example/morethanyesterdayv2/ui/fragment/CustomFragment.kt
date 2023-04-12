1d0040d8955b4939175b1228d187cd197f0353e6
vaneseA <spol@naver.com>
2023-04-12 09:30:30 +0900

    임시커밋

--- app/src/main/java/com/example/morethanyesterdayv2/ui/fragment/CustomFragment.kt
+++ app/src/main/java/com/example/morethanyesterdayv2/ui/fragment/CustomFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -49,7 +50,14 @@ class CustomFragment: Fragment() {
         exerciseListAdapter.exerciseDataList = exerciseDataList
 
         recyclerView.adapter = exerciseListAdapter
-
+        binding.customAddBtn.setOnClickListener {
+            // 저장 완료 메시지를 띄우고 다이얼로그를 닫음
+            Toast.makeText(
+                requireContext(),
+                "구현 준비중.",
+                Toast.LENGTH_LONG
+            ).show()
+        }
         // exerciseDataList 초기화
         exerciseDataList.clear()
         // 운동 데이터 추가
